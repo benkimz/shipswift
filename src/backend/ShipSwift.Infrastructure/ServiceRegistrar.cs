e351b8f8d628d94166b08aa937067a06424c60f1
benkimz <benkim3619@gmail.com>
2024-04-20 11:11:51 +0300

    Add service registrar

---
+++ src/backend/ShipSwift.Infrastructure/ServiceRegistrar.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ShipSwift.CoreBusiness;
+using ShipSwift.CoreBusiness.Models;
+
+namespace ShipSwift.Infrastructure;
+
+public static class ServiceRegistrar
+{
+    public static void AddApplicationService(this IServiceCollection services)
+    {
+        services.AddScoped<IGenericRepository<Shipper>, ShippersRepository>();
+    }
+}
